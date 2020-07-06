16c9d6ab6eb7ff161210ce25f15c70f760c71e96
Tomasz Dzieniak <tomasz.dzieniak@windly.co>
2020-07-06 09:28:59 +0200

    Add shortcut methods for Long

--- limbo/utility/src/main/java/co/windly/limbo/utility/primitives/builtins.kt
+++ limbo/utility/src/main/java/co/windly/limbo/utility/primitives/builtins.kt
@@ -45,3 +45,49 @@ fun Int?.orOne(): Int =
   this ?: Int.ONE
 
 //endregion
+
+//region Long
+
+/**
+ * The zeroed Long.
+ */
+val Long.Companion.ZERO: Long
+  get() = 0L
+
+/**
+ * A handy method that allows to check if given Long is equal to zero.
+ * Useful especially for lambda operations or higher level functions.
+ */
+fun Long.isZero(): Boolean =
+  this == Long.ZERO
+
+/**
+ * A null-safe shortcut elvis-alike method that returns the
+ * identity value (for non-null Long) or zero if given value is
+ * null.
+ */
+fun Long?.orZero(): Long =
+  this ?: Long.ZERO
+
+/**
+ * An Long that is equal to one.
+ */
+val Long.Companion.ONE: Long
+  get() = 1L
+
+/**
+ * A handy method that allows to check if given Long is equal to one.
+ * Useful especially for lambda operations or higher level functions.
+ */
+fun Long.isOne(): Boolean =
+  this == Long.ONE
+
+/**
+ * A null-safe shortcut elvis-alike method that returns the
+ * identity value (for non-null Long) or one if given value is
+ * null.
+ */
+fun Long?.orOne(): Long =
+  this ?: Long.ONE
+
+//endregion
