8d23960dcf0ee7ec13d93aba0db8323359f76c8a
Tomasz Dzieniak <tomasz.dzieniak@windly.co>
2020-07-29 11:50:33 +0200

    Refactor lifecycle component to rely on lifecycle composite disposable

--- limbo/mvvm/src/main/java/co/windly/limbo/mvvm/lifecycle/LifecycleComponent.kt
+++ limbo/mvvm/src/main/java/co/windly/limbo/mvvm/lifecycle/LifecycleComponent.kt
@@ -4,23 +4,14 @@ import android.view.View
 import android.view.View.OnClickListener
 import androidx.databinding.DataBindingComponent
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.OnLifecycleEvent
+import co.windly.limbo.disposable.LifecycleCompositeDisposable
 import co.windly.limbo.mvvm.binding.ClickDataBinding
 import co.windly.limbo.utility.reactive.addSuccessTo
 import co.windly.limbo.utility.reactive.throttledClicks
 import com.jakewharton.rxbinding3.view.longClicks
-import io.reactivex.disposables.CompositeDisposable
 
-class LifecycleComponent(private val lifecycle: Lifecycle) : DataBindingComponent,
-  LifecycleObserver {
-
-  //region Disposables
-
-  private val disposables: CompositeDisposable =
-    CompositeDisposable()
-
-  //endregion
+class LifecycleComponent(lifecycle: Lifecycle) : DataBindingComponent,
+  LifecycleCompositeDisposable(lifecycle, Lifecycle.Event.ON_DESTROY) {
 
   //region Click
 
@@ -41,34 +32,6 @@ class LifecycleComponent(private val lifecycle: Lifecycle) : DataBindingComponen
           .addSuccessTo(disposables) { onLongClick.onClick(view) }
       }
     }
-  }
-
-  //endregion
-
-  //region Dispose
-
-  /**
-   * Disposes all registered reactive streams. This method is being
-   * called whenever lifecycle owner will emit on destroy event.
-   */
-  @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-  fun dispose() {
-
-    // Clear disposables.
-    disposables.clear()
-
-    // Remove this object from lifecycle observation.
-    lifecycle.removeObserver(this)
-  }
-
-  //endregion
-
-  //region Initialization
-
-  init {
-
-    // Subscribe this object as lifecycle observer.
-    lifecycle.addObserver(this)
   }
 
   //endregion
